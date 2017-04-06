91e39f6eaabb98bc128e15b0864786b2c46dc9d7
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-05 22:14:32 -0700

    creates array of EQ coordinates for pins

--- scripts/app.js
+++ scripts/app.js
@@ -8,19 +8,24 @@ $(document).on("ready", function() {
     dataType: "json",
     success: onSuccess
   });   // ajax
-});  // doc on ready
-  function onSuccess(responseData) {
+});  // document on ready
 
-    var output = responseData.features;
-    output.forEach(function(item, index) {
-    	var newOutput = output[index].properties.title;
-    	$("#info").append('<p>' + newOutput + '</p>');
-    	var findLatLng = output[index].geometry.coordinates;
-    	console.log(findLatLng);
-    })
-    // [0].properties.title;
-    // for each (var in output) {
-}; 
+function onSuccess(responseData) {
+	var output = responseData.features;
+	// coordinates starts with SF coordinates
+	var coord = [{lat: 37.78, lng: -122.44}]; 
+	output.forEach(function(item, index) {
+		var newOutput = output[index].properties.title;
+		$("#info").append('<p>' + newOutput + '</p>');
+		var findLatLng = output[index].geometry.coordinates;
+		console.log(findLatLng);
+		// now take data coordinates in array, reverse order, and put into object
+		var arrLatLong = {} ;
+		arrLatLong['lat'] = output[index].geometry.coordinates[1];
+		arrLatLong['lng'] = output[index].geometry.coordinates[0];
+		coord.push(arrLatLong) ;
+	})   // output.forEach
+} // onSuccess
 
 var sfLatLng = {lat: 37.78, lng: -122.44};
 function initMap() {
